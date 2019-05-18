97d36cc874a6171a63d433209db69f38f8a0192a
markchu <markchu929@gmail.com>
2019-05-18 14:30:16 +0800

    first commit

---
+++ Week1HomeWork/DataTypeAttributes/行動電話驗證.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Week1HomeWork.DataTypeAttributes
+{
+    public class 行動電話驗證Attribute : DataTypeAttribute
+    {
+        public 行動電話驗證Attribute() : base(DataType.Text)
+        {
+            ErrorMessage = "格式錯誤:必須為 xxxx-xxxxxx !!";
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            string str = (string)value;
+
+            Regex rgx = new Regex(@"\d{4}-\d{6}$");
+
+            return rgx.IsMatch(str);
+        }
+
+    }
+}
